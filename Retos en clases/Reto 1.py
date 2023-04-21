dd3e309ed2b367325ef9107cee7bf1be44eb02dd
Miya134 <Miya134@users.noreply.github.com>
2023-04-21 11:11:37 -0400

    Commit 21/04

---
+++ Retos en clases/Reto 1.py
@@ -0,0 +1,9 @@
+#Problema 1
+
+palabra = input("Ingrese una palabra: ")
+vocales = ["a", "e", "i", "o", "u"]
+#count: Recibe un elemento como argumento, y cuenta la cantidad de veces que aparece en la lista.
+contador = [palabra.count(v) for v in vocales] 
+
+for i in range(len(vocales)):
+    print(f"la vocal {vocales[i]} aparece {contador[i]}")
